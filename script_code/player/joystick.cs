dbe789ac0b29071f5446e780f81674bf827adafc
emilyjiminroh <emilyjiminroh@gmail.com>
2022-01-14 01:32:06 +0900

    day 13 code

--- script_code/player/joystick.cs
+++ script_code/player/joystick.cs
@@ -1,69 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-// 1. 스틱 드래그 + 제한
-// 2. 드래그한만큼 캐릭터 이동
-
-public class JoyStick : MonoBehaviour
+public class PlayerCtrl : MonoBehaviour
 {
-    public RectTransform stick, backGround;
+    public GameObject joyStick, mainView, missionView;
+    public Settings settings_script;
+    public Button Btn;
 
-    PlayerCtrl playerCtrl_script;
     Animator anim;
+    GameObject coll;
+    
 
-    bool isDrag;
-    float limit;
+    //speed 변수 생성
+    public float speed;
 
+    public bool isCantMove;
+
+
+    //시작할 때의 실행
     private void Start()
     {
-        playerCtrl_script = GetComponent<PlayerCtrl>();
         anim = GetComponent<Animator>();
 
-        limit = backGround.rect.width * 0.5f;
+        // main camera를 Character의 자식으로 지정
+        Camera.main.transform.parent = transform;
+        // 처음 위치 지정 => 유니티에서의 main camera의 원래 위치를 상쇄해주기 위해
+        Camera.main.transform.localPosition = new Vector3(0, 0, -10);
     }
-
     private void Update()
     {
-        if (isDrag)
+        if (isCantMove)
         {
-            //드래그 하는 동안
-            Vector2 vec = Input.mousePosition - backGround.position;
-            //제한 두기
-            stick.localPosition = Vector2.ClampMagnitude(vec, limit);
+            joyStick.SetActive(false);
+        }
+        else
+        {
+            //move함수 호출
+            Move();
+        }
+        
+    }
 
-            Vector3 dir = (stick.position - backGround.position).normalized;
-            transform.position += dir * playerCtrl_script.speed * Time.deltaTime;
-
-            //animation 의 isWalk를 true로
-            anim.SetBool("isWalk", true);
-
-            //왼쪽으로 이동
-            if (dir.x < 0)
+    // 캐릭터 움직임 관리 함수
+   void Move()
+   {
+        if (settings_script.isJoyStick)
+        {
+            joyStick.SetActive(true);
+        }
+        else
+        {
+            joyStick.SetActive(false);
+            //JoyStick 말고 화면을 클릭했는지 판단
+            if (Input.GetMouseButton(0)) //좌측을 클릭 혹은 터치 했을 때
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    //dir 변수에 클릭한 위치 - 중간 위치를 빼주고 normalized 해준다
+                    Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f)).normalized;
+                    //위치를 speed 값과 시간을 고려해 업데이트 해준다
+                    transform.position += dir * speed * Time.deltaTime;
+
+                    //animation 의 isWalk를 true로
+                    anim.SetBool("isWalk", true);
+
+                    //왼쪽으로 이동
+                    if (dir.x < 0)
+                    {
+                        transform.localScale = new Vector3(-1, 1, 1);
+                    }
+                    //오른쪽으로 이동 (좌우반전)
+                    else
+                    {
+                        transform.localScale = new Vector3(1, 1, 1);
+                    }
+                }
+               
             }
-            //오른쪽으로 이동 (좌우반전)
+            //클릭하지 않는다면
             else
             {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-
-            // 버튼에서 마우스를 떼면 => 드래그 끝나면
-            if (Input.GetMouseButtonUp(0))
-            {
-                //부모요소 기준으로 위치를 맞춤
-                stick.localPosition = new Vector3(0, 0, 0);
                 anim.SetBool("isWalk", false);
-
-                isDrag = false;
             }
+        }
+
+       
+   }
+
+    //캐릭터 삭제
+    public void DestroyPlayer()
+    {
+        Camera.main.transform.parent = null;
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Mission")
+        {
+            coll = collision.gameObject;
+            Btn.interactable = true;
         }
     }
 
-    //스틱을 누르면 호출
-    public void ClickStick()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        isDrag = true;
+        if (collision.tag == "Mission")
+        {
+            coll = null;
+
+            Btn.interactable = false;
+        }
+    }
+    // 버튼 누르면 호출
+    public void ClickButton()
+    {
+        //MissionStart 호출
+        coll.SendMessage("MissionStart");
+        isCantMove = true;
+        Btn.interactable = false;
+
+    }
+
+    // 미션 종료하면 호출
+    public void MissionEnd()
+    {
+        isCantMove = false;
     }
 }
+
+
+
--- script_code/player/mission1.cs
+++ script_code/player/mission1.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Mission1 : MonoBehaviour
 {
+    public Color red;
+    public Image[] images;
+
     Animator anim;
     PlayerCtrl playerCtrl_script;
 
@@ -17,6 +22,20 @@ public class Mission1 : MonoBehaviour
     {
         anim.SetBool("isUp", true);
         playerCtrl_script = FindObjectOfType<PlayerCtrl>();
+
+        //초기화
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].color = Color.white;
+        }
+
+        //랜덤
+        for (int i = 0; i < 4; i++)
+        {
+            int rand = Random.Range(0, 7);
+
+            images[rand].color = red;
+        }
     }
 
     //엑스 버튼 누르면 호출
@@ -26,5 +45,47 @@ public class Mission1 : MonoBehaviour
         playerCtrl_script.MissionEnd();
     }
 
-    
+    //육각형 버튼 누르면 호출
+    public void ClickButton()
+    {
+        Image img = EventSystem.current.currentSelectedGameObject.GetComponent<Image>();
+
+        // 하얀색
+        if(img.color == Color.white)
+
+        {
+            // 빨간색으로
+            img.color = red;
+        }
+        //빨간색
+        else
+        {
+            //하얀색
+            img.color = Color.white;
+        }
+
+        // 성공여부 체크
+        int count = 0;
+
+        for(int i = 0; i < images.Length; i++)
+        {
+            if(images[i].color == Color.white)
+            {
+                count++;
+            }
+        }
+        if(count == images.Length)
+        {
+            //성공
+            Invoke("MissionSuccess", 0.2f); 
+        }
+
+    }
+
+    //미션 성공하면 호출
+    public void MissionSuccess()
+    {
+        ClickCancle();
+    }
+
 }
